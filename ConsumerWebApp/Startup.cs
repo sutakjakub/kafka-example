5acba0c6af2dc5dc5e3ea4a65ec5a782d696706f
sutakjakub <sutak.jakub@gmail.com>
2021-07-11 19:41:34 +0200

    init

---
+++ ConsumerWebApp/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsumerWebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Database connection string.
+            // Make sure to update the Password value below from "Your_password123" to your actual password.
+            var connection = @"Server=db;Database=master;User=sa;Password=Your_password123;";
+
+            // This line uses 'UseSqlServer' in the 'options' parameter
+            // with the connection string defined above.
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(connection));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
---
+++ KafkaExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DoConsumeJobAsync().Wait();
+            DoProduceJobAsync().Wait();
+        }
+
+        private static bool cancelled;
+
+        private static async Task DoConsumeJobAsync()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "Foo",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var timer = new Timer(TimerTick, null, 15000, 1);
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                try
+                {
+                    consumer.Subscribe(new[] { "testdataother" });
+
+                    while (!cancelled)
+                    {
+                        var consumeResult = consumer.Consume();
+                        Console.WriteLine($"Cosunmer: {consumeResult.Message.Value}");
+                    }
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+
+        private static void TimerTick(object state)
+        {
+            cancelled = true;
+        }
+
+        private static async Task DoProduceJobAsync()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = Dns.GetHostName()
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var task = producer.ProduceAsync("testdataother", new Message<Null, string>() { Value = "a log message" });
+                await task.ContinueWith((t) =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        Console.WriteLine($"Error occured");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrote offset: {t.Result.Offset}");
+                    }
+                });
+            }
+        }
+    }
+}
